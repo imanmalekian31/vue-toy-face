882688623132825fc5b78336967e654f2021799e
iman malekian <imanmalekian31@gmail.com>
2022-08-09 22:48:43 +0430

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# vue-toy-face
+
+<p align="center">
+<img width="648" src="https://user-images.githubusercontent.com/58827166/183727670-e5909d83-0e15-431c-af05-d626ab1a8492.png">
+</p>
+
+## Demo
+
+[Demo](https://vue-toy-face.vercel.app/)
+
+## Installation
+
+```shell
+# npm
+$ npm install vue-toy-face
+# yarn
+$ yarn add vue-toy-face
+```
+
+# License
+
+[The MIT License](http://opensource.org/licenses/MIT)
---
+++ src/components/VueToyFace.vue
@@ -0,0 +1,50 @@
+<template>
+  <span :style="avatarStyle" />
+</template>
+
+<script>
+const ROWS = 3;
+const COLS = 6;
+
+export default {
+  props: {
+    size: {
+      type: [Number, String],
+      default: 48,
+    },
+    rounded: {
+      type: [Number, String],
+      default: 0,
+    },
+    toyNumber: {
+      type: [Number, String],
+      default: 1,
+      validator(value) {
+        return value >= 1 && value <= 18;
+      },
+    },
+  },
+  computed: {
+    posX() {
+      return (this.toyNumber - 1) % COLS;
+    },
+    posY() {
+      return Math.floor((this.toyNumber - 1) / COLS);
+    },
+    avatarStyle() {
+      return {
+        display: "inline-block",
+        width: this.size + "px",
+        height: this.size + "px",
+        backgroundImage: "url(/Banner.jpeg)",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: this.size * COLS + "px " + this.size * ROWS + "px",
+        margin: "8px",
+        borderRadius: this.rounded + "px",
+        backgroundPositionX: `-${this.posX * this.size}px`,
+        backgroundPositionY: `-${this.posY * this.size}px`,
+      };
+    },
+  },
+};
+</script>
